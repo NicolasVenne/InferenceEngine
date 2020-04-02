10157f6b082e5efecdca871adc2660be9e1e430c
Nicolas Venne <nvenne@laurentian.ca>
2020-04-02 12:53:17 -0400

    Added comments

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var admin = require('firebase-admin');
 var AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 module.exports = InferenceEngine;
 
-
+/**
+ * Inference Engine Construstor
+ * @param {String} serviceAccount 
+ * @param {String} dbURL 
+ */
 function InferenceEngine(serviceAccount, dbURL) {
     if(typeof(serviceAccount) == "undefined") throw new Error("Not a valid firebase service account");
     if(typeof(dbURL) == "undefined") throw new Error("No database url supplied");
+    //Try to connect to the firebase database
     try {
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
@@ -15,15 +20,21 @@ function InferenceEngine(serviceAccount, dbURL) {
     } catch(e) {
         throw new Error("Failed to initialize connection to firebase")
     }
+    //Instance variable for the database.
     const _db = admin.firestore();
+    //Instance variable for the knowledge base.
     this._kb = _db.doc("knowledgebase_data/knowledgebase");
+    //Boolean to check if the callback from prompts has been attached.
     this._subscribedToPrompts = false;
-    this._jobs = [];    
-    this._infering = false;
     
 }
 
+/**
+ * Tell the engine to subscribe to update from the database.
+ */
 InferenceEngine.prototype.subscribeToKnowledge = async function() {
+    //If any other instance of this engine is running and updates the knowledge base,
+    //It will update the local varibles on this instance.
     return new Promise((resolve, reject) => {
         this._kbObserver = this._kb.onSnapshot(docSnapshot => {
             this._facts = docSnapshot.data().facts;
@@ -37,64 +48,81 @@ InferenceEngine.prototype.subscribeToKnowledge = async function() {
     })
 }
 
-// left leg hurts, red skin
-// ['/body/skin/red','/body/skin/purple']
 
+/**
+ * Ask the inference engine a question
+ * @param {String} input A list of comma separated values.
+ * @param {String} id The ID of the current conversation, for async operations.
+ */
 InferenceEngine.prototype.ask = async function(input, id = "") {
 
+    //Parse the comma separeated values into an array.
     let parsed = await this.parseFacts(input, id)
-
+    //An array of rules that are attached to the parsed facts.
     let possibleRules = {};
 
 
     for(let pFact in parsed) {
         for(let possibleRule in parsed[pFact]) {
 
+            //Lookup each parsed fact and add every relative rule to possibleRules 
+            //If the rule doesnt exist in the possibleRules, create it
             if(typeof possibleRules[parsed[pFact][possibleRule]] == "undefined") {
+                if(parsed[pFact][possibleRule] === "") {
+                    continue;
+                }
                 possibleRules[parsed[pFact][possibleRule]] = {
                     factCount: 0,
                     totalFacts: this._rules[parsed[pFact][possibleRule]].length
                 };
 
             } 
+            //And augment the current factCount that is attached to the rule to calculate certainty
             let temp = possibleRules[parsed[pFact][possibleRule]];
             temp.factCount++;
         }
     }
 
+    //Calculate the certainty of each rule found
     for(let pRule in possibleRules) {
         let rule = possibleRules[pRule];
         rule.certainty = rule.factCount / rule.totalFacts;
     }
 
 
+    //If they subscribed to the prompts
     if(this._prompt instanceof Function) {
         setTimeout(() => {
+            //Prompt if they found their rule
             this._prompt({type: "select", options: ["yes", "no"], message: "Find your diagnosis?"}, id).then((response) => {
+                //If they couldnt find it, ask for what rule it is.
                 if(response === "no") {
                     this._prompt({type: "input", message: "What is your diagnosis?"},id).then((response) => {
                         let array = []
+                        //Push all the symptoms to the given rule.
                         for(let key in parsed) {
                             array.push(key);
                         }
                         this._rules[response] = array;
+                        //Relate the newly created rule to its symptoms
                         this.inferFromRule(response);
                         this.updateDatabase();
                         this._prompt(0, id);
                     })
                 } else {
+                    //End convo
                     this._prompt(0, id);
                 }
             })
         }, 2000)
     }
-    
-    
     return possibleRules;
-
-
 }
 
+/**
+ * Relate the symptoms to the given rule
+ * @param {String} rule Name of the rule to relate
+ */
 InferenceEngine.prototype.inferFromRule = function(rule) {
     let r = this._rules[rule];
 
@@ -112,53 +140,46 @@ InferenceEngine.prototype.inferFromRule = function(rule) {
     }
 }
 
+/**
+ * Update the local knowledgebase to the online one.
+ */
 InferenceEngine.prototype.updateDatabase = function() {
     this._kb.update({facts: this._facts, rules: this._rules});
 }
 
-
+/**
+ * Parse the comma seperated facts into an array.
+ * @param {String} input List of comma seperated facts.
+ * @param {String} id The current id of the convo
+ */
 InferenceEngine.prototype.parseFacts = async function(input, id) {
+    //Make array from comma seperated facts
     let unparsedFacts = input.split(",");
     let parsedFacts = [];
 
+    //Loop through all the input facts
     for(let index in unparsedFacts) {
         let unparsedFact = unparsedFacts[index];
+        //Fact could be more then one word e.g skin red
+        //Split it into individual words
         let complexFact = unparsedFact.split(" ");
 
         //Search all facts if part of input fact is contained in it
         var result = {result: []};
         searchFacts(this._facts, complexFact.pop(), result);
 
+        //If there is more then one word, search again if there was no results for the first word.
         while(complexFact.length != 0 && result.result.length == 0) {
             searchFacts(this._facts, complexFact.pop(), result);
         }
 
-        
-
-        if(result.result.length == 0) {
-            //Check for synonyms 
-
-            let synonyms = checkSynonyms(this._synonyms, unparsedFact);
-
-            if(synonyms.length != 0) {
-                searchFacts(this._facts, synonyms.pop(), result);
-            } else {
-                while(result.result.length == 0) {
-                    let msg = await this._prompt({type: "input", message: `I don't understand: ${unparsedFacts[index]}`}, id);
-                    if(msg == 0) {
-                        break;
-                    }
-                    synonyms = checkSynonyms(this._synonyms, msg);
-                    searchFacts(this._facts, synonyms.pop(), result);
-                }
-                if(result.result.length == 0) {
-                    continue;
-                } 
-            }
-        }
 
         result = result.result;
 
+        //Searching can return more than one value,
+        //e.g searching for red skin,
+        //it will search for "skin" and return cold skin and red skin.
+        //But we only want red skin therefore we take out the bad results.
         let badResults = [];
         while(complexFact.length != 0) {
             let complex = complexFact.pop();
@@ -170,10 +191,12 @@ InferenceEngine.prototype.parseFacts = async function(input, id) {
             }
         }
 
+        //Get the array difference of all the search facts and the bad results
+        //Will give you the good Facts
         let goodFacts = arrayDiff(result, badResults);
 
         if(goodFacts.length == 0) {
-            //no good facts, prompt from upper directory of bad results.
+            throw new Error("Could not find fact");
         }
 
 
@@ -181,6 +204,7 @@ InferenceEngine.prototype.parseFacts = async function(input, id) {
         parsedFacts.push(...goodFacts)
     }
 
+    //Attach allt he parsedFacts to an output array to return
     let output = {};
     for(let parsedFact in parsedFacts) {
         let scan = parsedFacts[parsedFact].split("/");
@@ -191,33 +215,15 @@ InferenceEngine.prototype.parseFacts = async function(input, id) {
         }
         output[parsedFacts[parsedFact]] = find;
     }
-
     return output;
 }
 
-function checkSynonyms(synonyms, input) {
-    let output = [];
-    let breakOut = false;
-    let check = input.split(' ');
-    while(check.length != 0) {
-        let checkVal = check.pop();
-        for(let key in synonyms) {
-            for(let syn in synonyms[key]) {
-                if(checkVal == synonyms[key][syn] || checkVal == key) {
-                    output.push(key);
-                    breakOut = true;
-                    break;
-                }
-            }
-            if(breakOut) {
-                breakOut = false;
-                break;
-            }
-        }
-    }
-    return output;
-}
 
+/**
+ * Get the subset difference of a1 - a2
+ * @param {Array} a1 The main array
+ * @param {Array} a2 The array to substract from a1
+ */
 function arrayDiff (a1, a2) {
 
     var a = [], diff = [];
@@ -241,12 +247,25 @@ function arrayDiff (a1, a2) {
     return diff;
 }
 
+/**
+ * A recursive function to search a tree for facts
+ * @param {Array} facts list of all facts
+ * @param {String} complex the search keyword
+ * @param {Array} result the output
+ */
 function searchFacts(facts, complex, result) {
     let current = "";
 
     searchFactsRec(facts, complex, current, result);
 }
 
+/**
+ * The recursive function that is called from search facts
+ * @param {*} facts list of all facts passed from searchFacts
+ * @param {*} complex the search keyword passed from searchFacts
+ * @param {*} current the current level in the tree
+ * @param {*} result the output passed from searchFacts
+ */
 function searchFactsRec(facts, complex, current, result) {
     for(let n in facts) {
         if(n == complex) {
@@ -266,11 +285,12 @@ function searchFactsRec(facts, complex, current, result) {
     }
 }
 
-InferenceEngine.prototype.tell = async function (name, value) {
-    
-
-}
-
+/**
+ * Listen for when the engine needs additional information and will call
+ * the prompt function passed in
+ * @param {Function} prompt The callback function to call for prompts
+ *                          This function requires a message object and optional id.
+ */
 InferenceEngine.prototype.subscribeToPrompts = async function (prompt) {
     if(prompt instanceof AsyncFunction) {
         this.subscribeToPrompts = true;
@@ -279,58 +299,10 @@ InferenceEngine.prototype.subscribeToPrompts = async function (prompt) {
     
 }
 
-InferenceEngine.prototype.assertRule = async function(name, rule) {
 
-    
-    
-}
 
 
  
-InferenceEngine.prototype.infer = async function() {
-    // this._infering = true;
 
-    // if(this._jobs.length == 0) {
-    //     this._infering = false;
-    // }
-
-    // let rule = this._jobs.shift();
-
-    // if(this._infering) {
-    //     try {
-            
-    //         let snapshot = null;
-    //         try {
-    //             snapshot = await this._rules.doc(rule).get();
-    //         } catch(e) {
-    //             return undefined;
-    //         }
-            
-    //         let ruleFunction = new AsyncFunction("",snapshot.data().body);
-    //         ruleFunction.call(this);
-
-    //         this.infer();
-    //     } catch(e) {
-    //         this._infering = false;
-    //     }
-    // }
-
-    
-}
-
-// function getDocPath(doc) {
-//     if(doc.charAt(0) !== "/") {
-//         doc = "/" + doc;
-//     }
-//     if(doc.split("/").length - 1 > 1) {
-//         let paths = doc.split("/");
-//         paths.shift();
-//         paths[0] = "/" + paths[0];
-//         return paths;
-//     } else {
-//         return [doc];
-//     }
-
-// }
 
 
